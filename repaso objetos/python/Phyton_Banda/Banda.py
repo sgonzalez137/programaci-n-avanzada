59d2343e86ad6f6024cb3d53a960d03460ef4ad8
Miguel <mavelozao@correo.udistrital.edu.co>
2020-10-08 14:40:31 -0500

    cambios

--- repaso objetos/python/Phyton_Banda/Banda.py
+++ repaso objetos/python/Phyton_Banda/Banda.py
@@ -11,21 +11,7 @@ class Banda():
         m=Musico()
         m.set__nombre__(nombre)
         self.musicos.append(m)  
-
-    def generarInstrumento(self):
-        rn = random.randint(0,3)
-        sel = rn
-        i=Instrumento()
-        if sel == 1:
-            i= Guitarra()
-            return i
-        elif sel == 2:
-            i= Bajo()
-            return i
-        else:
-            i= Violin()
-            return i
-        return i   
+ 
     def generarRandom(self):
         a= random.randint(0,2) 
         return a  
@@ -36,13 +22,14 @@ class Banda():
             i.presentar()
             rn = random.randint(0,2)
             sel = a.generarRandom()
+
             if sel == 1:
                 j= Guitarra()
             elif sel == 2:
                 j= Bajo()         
             else:
                 j= Violin() 
-            print(sel)
+            
             
             
             i.tocar(j)
