54273a8087d7e268135c024e91074ff4d6086cf7
Jose Alejandro Cortazar Lopez <alejohermes@gmail.com>
2020-11-12 13:48:13 -0500

    juego sintetizado

---
+++ pikasyfijas/readme.mk
@@ -0,0 +1,8 @@
+JOSE ALEJANDRO CORTAZAR LOPEZ   20181020022
+SEBASTIAN GONZALEZ              20182020070
+MIGUEL VELOSA                   20192020012
+
+
+
+este programa es laejecucion del juego pikas y fijas, con etura de archivos
+
